37d3382e16acbc56d755680267a7848426f011d8
dbh <sessak02@naver.com>
2026-01-11 15:55:16 +0900

    회원가입 오류코드 수정

--- src/main/java/com/mysite/doomchit/users/UserController.java
+++ src/main/java/com/mysite/doomchit/users/UserController.java
@@ -1,16 +1,21 @@
 package com.mysite.doomchit.users;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-@RequestMapping("/doomchit/*")
+@RequestMapping("/doomchit")
 @RequiredArgsConstructor
 @Controller
 public class UserController {
 
+	// ----------------------------------------------------
+	// Controller : 요청 / 응답, Validation 결과 처리, 에러 종류 결정
+	// ----------------------------------------------------
+	
     private final UserService userService;
 
     // 회원가입 ===========================================
@@ -22,16 +27,49 @@ public class UserController {
     @PostMapping("/signup")
     public String signup(@Valid UserForm userForm, BindingResult bindingResult) {
     	
+    	// 기본 Validation
         if (bindingResult.hasErrors()) {
             return "signup";
         }
         
+        // 비밀번호 확인 체크
         if(!userForm.getUserPwd1().equals(userForm.getUserPwd2())) {
         	bindingResult.rejectValue("userPwd2", "passwordIncorrect", "비밀번호가 일치하지 않습니다.");
         	return "signup";
         }
         
-        userService.create(userForm.getUserId(), userForm.getUserPwd1(), userForm.getUsername());
+        // UX용 아이디 중복 체크
+        if (userService.existsUserId(userForm.getUserId())) {
+            bindingResult.rejectValue(
+                "userId",
+                "duplicate",
+                "이미 사용 중인 아이디입니다."
+            );
+            return "signup";
+        }
+        
+        // UX용 닉네임 중복 체크
+        if (userService.existsUsername(userForm.getUsername())) {
+            bindingResult.rejectValue(
+                "username",
+                "duplicate",
+                "이미 사용 중인 닉네임입니다."
+            );
+            return "signup";
+        }
+        
+        // 회원가입 시도
+        try {
+        	userService.create(
+        			userForm.getUserId(), userForm.getUserPwd1(), userForm.getUsername()
+        			);
+        } catch(DataIntegrityViolationException e) {
+        	bindingResult.rejectValue("userId", "duplicate", "중복된 값이 존재합니다.");
+        	
+        	return "signup";
+        	
+            }
+        
         return "redirect:/";
     }
     
