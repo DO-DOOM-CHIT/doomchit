e596bcaf03545f8ed0a06a858f54b64ff4610bff
dbh <sessak02@naver.com>
2026-01-11 16:56:48 +0900

    ERD수정, 리뷰 DB 수정

--- src/main/java/com/mysite/doomchit/reviews/Review.java
+++ src/main/java/com/mysite/doomchit/reviews/Review.java
@@ -1,5 +1,6 @@
 package com.mysite.doomchit.reviews;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -17,13 +18,22 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "reviews")
+// 한 유저가 하나의 음악에 여러 개의 리뷰를 달지 못하게 하는 제약
+@Table(name = "reviews", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "uk_reviews_user_music",
+                columnNames = {"uno", "mno"}
+            )
+        })
 public class Review {
 
   // 리뷰 고유 번호
@@ -34,6 +44,12 @@ public class Review {
   // 리뷰 내용
   @Column(columnDefinition = "TEXT", nullable = false)
   private String content;
+  
+  // 평점 (1.0 ~ 5.0, 0.5 단위)
+  @DecimalMin(value = "1.0", inclusive = true)        // 최저점 1.0
+  @DecimalMax(value = "5.0", inclusive = true)        // 최고점 5.0
+  @Column(precision = 2, scale = 1, nullable = false) // 0.5단위
+  private BigDecimal rating;
 
   // 리뷰 작성일
   @CreationTimestamp
