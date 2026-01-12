bf81b0cf60613fc124669b27cad0318585fc58d4
ElecVirtDev <evd94623@gmail.com>
2026-01-12 13:59:51 +0900

    약간의 코드 수정

--- src/main/resources/static/js/reviews.js
+++ src/main/resources/static/js/reviews.js
@@ -1,3 +1,15 @@
+function toggleLyrics() {
+  const content = document.getElementById("lyricsContent");
+  const btn = document.getElementById("lyricsBtn");
+  if (content.classList.contains("collapsed")) {
+    content.classList.remove("collapsed");
+    btn.innerHTML = '접기<i class="fa-solid fa-angle-up ps-1"></i>';
+  } else {
+    content.classList.add("collapsed");
+    btn.innerHTML = '펼치기<i class="fa-solid fa-angle-down ps-1"></i>';
+  }
+}
+
 const rating_value = document.querySelector("#rating_value");
 const rating_input = document.querySelector("#rating_input");
 if (rating_value && rating_input) {
